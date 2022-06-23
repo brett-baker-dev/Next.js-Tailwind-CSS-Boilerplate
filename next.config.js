43c7c3b36f977b2012021fac5142921ef756dace
brett-baker-dev <brett.baker.dev@gmail.com>
2022-06-22 22:18:04 -0700

    resolve path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve('./'));
+    return config;
+  },
 }
 
 module.exports = nextConfig
