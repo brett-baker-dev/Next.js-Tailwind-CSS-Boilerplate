94cad04af2f41a3cc2f3bb2c6a7e10204ca501ac
brett-baker-dev <brett.baker.dev@gmail.com>
2022-06-23 06:03:46 -0700

    add classnames for sass

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import classnames from 'classnames/bind'
+
+import * as styles from './index.module.scss'
+
+const cx = classnames.bind(styles)
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold">
+    <h1 className={"text-3xl font-bold " + cx("home")}>
       Hello world!
     </h1>
   )
